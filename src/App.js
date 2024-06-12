0a87c7c0041b801bd8d7968ac16b4d613a0e9e6f
singhsatyendra8052 <singhsatyendra8052@gmail.com>
2024-06-12 16:15:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./modules/Home";
+import { Routes, Route } from "react-router-dom";
+import Product from "./modules/Product";
+import Products from "./modules/Products";
+import CategoryProducts from "./modules/CategoryProducts";
+import Cart from "./modules/Cart";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/categories/:name" element={<CategoryProducts />} />
+        <Route path="cart" element={<Cart />} />
+        <Route
+          path="*"
+          element={
+            <div className="h-[50vh] text-4xl tex-bold flex flex-row justify-center items-center">
+              404 not found{" "}
+            </div>
+          }
+        />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
